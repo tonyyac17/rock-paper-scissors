ca7a5fba0565141b683b0443908ae1c53140d4e0
Tony Yacovetta <tony.yacovetta@gmail.com>
2023-12-01 12:35:03 -0600

    added button to html webpage started making js code to play rock papaer scissors game

--- javascript.js
+++ javascript.js
@@ -1,5 +1,34 @@
 console.log("Hello World")
 
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ''
+
+function playGame (computerSelection, playerSelection){
+    if(computerSelection === playerSelection)
+    roundWinner = 'tie'
+}
+    if(
+    (computerSelection === 'Rock' && playerSelection === 'Scissors')||
+    (computerSelection === 'Scissors' && playerSelection === 'Paper')||
+    (computerSelection === 'Paper' && playerSelection === 'Rock')
+    ){
+        computerScore++
+        roundWinner = 'You lose.' + computerSelection + ' beats ' + playerSelection
+    }
+
+
+    if(
+    (playerSelection === 'Rock' && computerSelection === 'Scissors')||
+    (playerSelection === 'Paper' && computerSelection === 'Rock')||
+    (playerSelection === 'Scissors' && computerSelection === 'Paper') 
+    ){
+        playerScore++
+        roundWinner = 'You win this round!' + playerSelection + ' beats ' + computerSelection
+    }
+
+
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random()*3);
     switch (randomNumber) {
